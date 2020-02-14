c2d0fb1775b104d2474677704bea5a9d12d57b0e
Michal Stehlík <st@pslib.cloud>
2020-02-14 11:34:49 +0100

    links

--- GamesCollection/Pages/Details.cshtml.cs
+++ GamesCollection/Pages/Details.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using GamesCollection.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace GamesCollection
@@ -21,10 +22,16 @@ namespace GamesCollection
         }
 
         public Company Company { get; set; }
+        public List<Game> GamesPublished { get; set; } = new List<Game>();
+        public List<Game> GamesDeveloped { get; set; } = new List<Game>();
+        public List<Company> Subsidiaries { get; set; } = new List<Company>();
 
         public void OnGet(int id)
         {
-            Company = _context.Companies.Where(c=>(c.Id == id)).FirstOrDefault();
+            Company = _context.Companies.Include(c => c.Parent).Where(c=>(c.Id == id)).FirstOrDefault();
+            GamesPublished = _context.Games.Where(g => (g.PublisherId == id)).OrderBy(g => g.Name).ToList();
+            GamesDeveloped = _context.Games.Where(g => (g.DeveloperId == id)).OrderBy(g => g.Name).ToList();
+            Subsidiaries = _context.Companies.Where(c => (c.ParentId == id)).OrderBy(c => c.Name).ToList();
         }
     }
 }
--- GamesCollection/Pages/Game.cshtml.cs
+++ GamesCollection/Pages/Game.cshtml.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GamesCollection.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace GamesCollection
 {
     public class GameModel : PageModel
     {
+        private readonly ILogger<DetailsModel> _logger;
+        private ApplicationDbContext _context;
+        public Game Game { get; set; }
+        public GameModel(ILogger<DetailsModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
         public void OnGet(int id)
         {
-
+            Game = _context.Games
+                .Include(g => g.Developer)
+                .Include(g => g.Publisher)
+                .Where(g => (g.Id == id)).FirstOrDefault();
         }
     }
 }
